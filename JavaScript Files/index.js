a1bce586d4893c3705dd72fa0d667e2f33ebaecc
mdshakibsami <mdshakibsami21@gmail.com>
2025-03-01 14:41:24 +0600

    added some funcitonality

---
+++ JavaScript Files/index.js
@@ -0,0 +1,9 @@
+document.getElementById("discover-something").addEventListener('click',function()
+{
+    window.location.href="../blog.html";
+})
+
+document.getElementById("clear-btn").addEventListener("click", function()
+{
+    document.getElementById("history-list").innerText = "";
+})
---
+++ JavaScript Files/utilites.js
@@ -0,0 +1,5 @@
+function getInnerTextById(id)
+{
+    const value = document.getElementById(id).innerHTML;
+    return value;
+}
