624c8900e4aa6a635dd7e3600d8c835d2dba676e
mdshakibsami <mdshakibsami21@gmail.com>
2025-03-02 16:23:00 +0600

    made six button fully fuctional

--- JavaScript Files/index.js
+++ JavaScript Files/index.js
@@ -4,34 +4,128 @@ const date = now.getDate();
 const month = getMonthByName(now.getMonth());
 const year = now.getFullYear();
 const day = getDayName(now.getDay());
-const currentDate = month+" "+date+" "+year;
+const currentDate = month + " " + date + " " + year;
 
 // Set the Date
 document.getElementById("current-day").innerText = day;
 document.getElementById("current-date").innerText = currentDate;
 const dateClassList = document.getElementsByClassName("box-date");
-console.log(dateClassList);
-for(const date of dateClassList)
-{
-    date.innerHTML = currentDate;
+for (const date of dateClassList) {
+  date.innerHTML = currentDate;
 }
 
 // switching page
-document.getElementById("discover-something").addEventListener('click',function()
-{
-    window.location.href="blog.html";
-})
-
+document
+  .getElementById("discover-something")
+  .addEventListener("click", function () {
+    window.location.href = "blog.html";
+  });
 
 // Clear Button
-document.getElementById("clear-btn").addEventListener("click", function()
-{
-    document.getElementById("history-list").innerText = "";
-})
+document.getElementById("clear-btn").addEventListener("click", function () {
+  document.getElementById("history-list").innerText = "";
+});
 
+//Create Time
+const hour = now.getHours();
+const minute = now.getMinutes();
+const second = now.getSeconds();
+let time = "";
+if (hour >= 12) {
+  time = hour + ":" + minute + ":" + second + " PM";
+} else {
+  time = hour + ":" + minute + ":" + second + " AM";
+}
+console.log(time);
 
-document.getElementById("shop-ease-btn").addEventListener('click',function()
-{
-    alert();
+// Six Buttons
+let counter = 0;
 
-})
+document.getElementById("btn-1").addEventListener("click", function () {
+  alert();
+  counter++;
+  setNotification(
+    "title-1",
+    "history-list",
+    "number-of-task",
+    "total-task",
+    time,
+    counter
+  );
+  document.getElementById("btn-1").disabled = true;
+
+});
+
+document.getElementById("btn-2").addEventListener("click", function () {
+  alert();
+  counter++;
+  setNotification(
+    "title-2",
+    "history-list",
+    "number-of-task",
+    "total-task",
+    time,
+    counter
+  );
+  document.getElementById("btn-2").disabled = true;
+
+});
+
+document.getElementById("btn-3").addEventListener("click", function () {
+  alert();
+  counter++;
+  setNotification(
+    "title-3",
+    "history-list",
+    "number-of-task",
+    "total-task",
+    time,
+    counter
+  );
+  document.getElementById("btn-3").disabled = true;
+
+});
+
+document.getElementById("btn-4").addEventListener("click", function () {
+  alert();
+  counter++;
+  setNotification(
+    "title-4",
+    "history-list",
+    "number-of-task",
+    "total-task",
+    time,
+    counter
+  );
+  document.getElementById("btn-4").disabled = true;
+
+});
+
+document.getElementById("btn-5").addEventListener("click", function () {
+  alert();
+  counter++;
+  setNotification(
+    "title-5",
+    "history-list",
+    "number-of-task",
+    "total-task",
+    time,
+    counter
+  );
+  document.getElementById("btn-5").disabled = true;
+});
+
+document.getElementById("btn-6").addEventListener("click", function () {
+  alert();
+  counter++;
+  setNotification(
+    "title-6",
+    "history-list",
+    "number-of-task",
+    "total-task",
+    time,
+    counter
+  );
+  document.getElementById("btn-6").disabled = true;
+
+});
--- JavaScript Files/utilites.js
+++ JavaScript Files/utilites.js
@@ -33,20 +33,60 @@ function getMonthByName(n) {
 }
 
 function getDayName(n) {
-    switch (n) {
-        case 0: return "Sunday";
-        case 1: return "Monday";
-        case 2: return "Tuesday";
-        case 3: return "Wednesday";
-        case 4: return "Thursday";
-        case 5: return "Friday";
-        case 6: return "Saturday";
-    }
+  switch (n) {
+    case 0:
+      return "Sunday";
+    case 1:
+      return "Monday";
+    case 2:
+      return "Tuesday";
+    case 3:
+      return "Wednesday";
+    case 4:
+      return "Thursday";
+    case 5:
+      return "Friday";
+    case 6:
+      return "Saturday";
+  }
+}
+
+function getInnerValueById(id) {
+  const value = document.getElementById(id).innerText;
+  return value;
+}
+function getInnerValueByIdAsInteger(id) {
+  const value = document.getElementById(id).innerText;
+  valueInt = parseInt(value);
+  return valueInt;
 }
 
 
-function getInnerValueById(id)
-{
-    const value = document.getElementById(id).innerText;
-    return value;
+function setNotification(titleId, historyDivId, numberOfTaskId, totalTaskId,time) {
+  const title = getInnerValueById(titleId);
+  const message = "You have Complete The Task " + title + " at " + time;
+  console.log(message);
+
+  const historyDiv = document.getElementById(historyDivId);
+
+  let div = document.createElement("div");
+  div.innerHTML = `<p
+              class="bg-[#F4F7FF] mt-2 py-2 px-4 border border-blue-500 rounded-lg text-justify"
+            >
+            ${message}
+          </p>
+    `;
+  historyDiv.appendChild(div);
+
+  let taskAssign = getInnerValueByIdAsInteger(numberOfTaskId);
+  taskAssign -= 1;
+  document.getElementById("number-of-task").innerText = taskAssign;
+
+  let totalTask = getInnerValueByIdAsInteger(totalTaskId);
+  totalTask += 1;
+  document.getElementById("total-task").innerText = totalTask;
+
+  if (counter == 6) {
+    alert("Congratulations!, You have completed all the task.");
+  }
 }
