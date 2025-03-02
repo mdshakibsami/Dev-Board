18f84148acf1ce4e60b224c342ce260f891c4eee
mdshakibsami <mdshakibsami21@gmail.com>
2025-03-02 15:12:47 +0600

    current date added

--- JavaScript Files/index.js
+++ JavaScript Files/index.js
@@ -1,9 +1,37 @@
+// Generating Date
+const now = new Date();
+const date = now.getDate();
+const month = getMonthByName(now.getMonth());
+const year = now.getFullYear();
+const day = getDayName(now.getDay());
+const currentDate = month+" "+date+" "+year;
+
+// Set the Date
+document.getElementById("current-day").innerText = day;
+document.getElementById("current-date").innerText = currentDate;
+const dateClassList = document.getElementsByClassName("box-date");
+console.log(dateClassList);
+for(const date of dateClassList)
+{
+    date.innerHTML = currentDate;
+}
+
+// switching page
 document.getElementById("discover-something").addEventListener('click',function()
 {
     window.location.href="blog.html";
 })
 
+
+// Clear Button
 document.getElementById("clear-btn").addEventListener("click", function()
 {
     document.getElementById("history-list").innerText = "";
+})
+
+
+document.getElementById("shop-ease-btn").addEventListener('click',function()
+{
+    alert();
+
 })
--- JavaScript Files/utilites.js
+++ JavaScript Files/utilites.js
@@ -1,5 +1,52 @@
-function getInnerTextById(id)
+function getInnerTextById(id) {
+  const value = document.getElementById(id).innerHTML;
+  return value;
+}
+
+function getMonthByName(n) {
+  switch (n) {
+    case 0:
+      return "January";
+    case 1:
+      return "February";
+    case 2:
+      return "March";
+    case 3:
+      return "April";
+    case 4:
+      return "May";
+    case 5:
+      return "June";
+    case 6:
+      return "July";
+    case 7:
+      return "August";
+    case 8:
+      return "September";
+    case 9:
+      return "October";
+    case 10:
+      return "November";
+    case 11:
+      return "December";
+  }
+}
+
+function getDayName(n) {
+    switch (n) {
+        case 0: return "Sunday";
+        case 1: return "Monday";
+        case 2: return "Tuesday";
+        case 3: return "Wednesday";
+        case 4: return "Thursday";
+        case 5: return "Friday";
+        case 6: return "Saturday";
+    }
+}
+
+
+function getInnerValueById(id)
 {
-    const value = document.getElementById(id).innerHTML;
+    const value = document.getElementById(id).innerText;
     return value;
 }
